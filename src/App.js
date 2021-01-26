7a81558665396106718aa8f86d40aa46d60feb61
prabhakarkmr0 <prabhakarkmr0@gmail.com>
2021-01-26 09:44:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React,{Component} from "react"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super()
+    this.state={time:new Date()}
+  }
+
+  liveTime(){
+    this.setState({
+      time:new Date()
+    })
+  }
+
+  componentWillMount(){
+    setInterval(()=>this.liveTime(),1000)
+  }
+
+
+  render(){
+    return(
+      <h1>
+        {this.state.time.toLocaleTimeString()}
+      </h1>
+    )
+
+  }
+
 }
 
 export default App;
